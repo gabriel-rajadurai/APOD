137c524cd66584d3f33ff2aa4f796d01b0a93bcb
grajadurai <grjadurai@rapidbizapps.com>
2020-08-10 12:14:58 +0530

    Expand/Collapse implementation

--- app/src/main/java/com/gabriel/astronomypod/common/Extensions.kt
+++ app/src/main/java/com/gabriel/astronomypod/common/Extensions.kt
@@ -1,6 +1,7 @@
 package com.gabriel.astronomypod.common
 
 import android.net.Uri
+import android.view.View
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
@@ -11,4 +12,12 @@ fun ImageView.loadUrl(url: String) {
         .centerCrop()
         .transition(DrawableTransitionOptions.withCrossFade())
         .into(this)
+}
+
+fun View.gone() {
+    visibility = View.GONE
+}
+
+fun View.visible() {
+    visibility = View.VISIBLE
 }
--- app/src/main/java/com/gabriel/astronomypod/features/apodList/ApodListAdapter.kt
+++ app/src/main/java/com/gabriel/astronomypod/features/apodList/ApodListAdapter.kt
@@ -7,11 +7,15 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.gabriel.astronomypod.R
+import com.gabriel.astronomypod.common.gone
 import com.gabriel.astronomypod.common.loadUrl
+import com.gabriel.astronomypod.common.visible
 import com.gabriel.data.models.APOD
 import kotlinx.android.synthetic.main.item_apod.view.*
 
 class ApodListAdapter : ListAdapter<APOD, ApodListAdapter.ViewHolder>(DIFF_CALLBACK) {
+
+    private var previousExpandedItem = -1
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
@@ -29,17 +33,29 @@ class ApodListAdapter : ListAdapter<APOD, ApodListAdapter.ViewHolder>(DIFF_CALLB
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(apod: APOD) {
             itemView.tvTitle.text = apod.title
+            if (previousExpandedItem == adapterPosition) itemView.infoLayout.visible()
+            else itemView.infoLayout.gone()
             if (apod.mediaType == APOD.MEDIA_TYPE_IMAGE) {
                 itemView.ivApod.loadUrl(apod.url)
-                itemView.ivDownload.visibility = View.VISIBLE
+                itemView.ivDownload.visible()
             } else {
                 itemView.ivApod.setImageResource(R.drawable.ic_play)
-                itemView.ivDownload.visibility = View.GONE
+                itemView.ivDownload.gone()
             }
             itemView.tvDate.text = apod.date
             itemView.tvDescription.text = apod.explanation
             itemView.tvTitle.setOnClickListener {
-                itemView.infoLayout.visibility = View.VISIBLE
+                if (previousExpandedItem == adapterPosition) {
+                    itemView.infoLayout.gone()
+                    previousExpandedItem = -1
+                    return@setOnClickListener
+                }
+
+                if (previousExpandedItem >= 0)
+                    notifyItemChanged(previousExpandedItem)
+
+                previousExpandedItem = adapterPosition
+                itemView.infoLayout.visible()
             }
         }
 
